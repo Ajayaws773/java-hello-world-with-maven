b48d53f5238c744512619c6eccdb69ca544482a4
Aj <97377832+Ajayaws773@users.noreply.github.com>
2024-05-13 11:49:02 +0530

    Create artifact.sh

---
+++ artifact.sh
@@ -0,0 +1,17 @@
+set +x
+LocalWorkspace=${WORKSPACE}
+WARFilePath="$LocalWorkspace/target/*.jar"
+echo "WAR File Path is ( Source path from the server) : $WARFilePath"
+
+foldername="Bpi-$(date +"%d-%m-%Y-%H-%M-%S")"
+mkdir -p "$LocalWorkspace/target/$foldername"
+echo "Folder is created"
+
+echo "Starting - copying file from $WARFilePath to $LocalWorkspace/target/$foldername/"
+cp $WARFilePath "$LocalWorkspace/target/$foldername/"
+echo "Completed - copying file from $WARFilePath to $LocalWorkspace/target/$foldername/"
+
+cd "$LocalWorkspace/target/"
+echo "Starting - Upload folder to S3 bucket"
+aws s3 cp $foldername "s3://cintap-artifact-test/builds/$foldername/" 
+echo "Completed - Upload folder to S3 bucket"
