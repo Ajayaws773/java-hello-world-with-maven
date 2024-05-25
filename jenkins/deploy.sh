89d72d7f620d764cdf6e0cd9bcae6353bce4bc69
Aj <97377832+Ajayaws773@users.noreply.github.com>
2024-05-25 22:57:37 +0530

    Create deploy.sh

---
+++ jenkins/deploy.sh
@@ -0,0 +1,59 @@
+set +x
+set -e
+
+Environment=$Environment
+component=$component
+
+#### VARIABLES - DECLARATION SECTION ######
+echo "########## The Deployment details are ........################ "
+echo "The Environment to Deploy is : $Environment"
+echo "The service to deploy is : $component"
+
+
+########## Declaring the server names by the environment ##########
+DevEnvServers=("100.26.153.234")
+UATUIServers=("172.31.40.153")
+UATb2bServers=("172.31.40.154")
+
+
+
+echo "##################################################################"
+TargetServersList=""
+if [[ $Environment == "Dev" && ]]
+then
+  echo "The Target environment is : Dev"
+  TargetServersList=("${DevEnvServers[@]}")
+elif [[ $Environment == "UAT" && $component== "ui"]]
+then
+  echo "The Target environment is : UATUI"
+  TargetServersList=("${UATUIServers[@]}")
+elif [[ $Environment == "UAT" && $component== "service"]]
+then
+  echo "The Target environment is : UAT"
+  TargetServersList=("${UATb2bServers[@]}")
+fi
+
+#TargetServersListCount=${#TargetServersList[@]}
+
+echo $TargetServersList
+#echo "Total Count of servers to be deployed is :$TargetServersListCount "
+
+# use for loop to read all values and indexes
+for value in "${TargetServersList[@]}" ; do    #print the new array 
+echo "$value" 
+done   
+echo "##################################################################"
+
+
+
+########## Starting for the loop if we have to deploy to multiple servers #########
+for TargetServer in "${TargetServersList[@]}" ; do
+
+
+
+
+
+### Copying latest files to Tomcat
+echo "### Starting - Copying WAR file(s) to the server : $TargetServer ###" 
+scp $WORKSPACE/target/*.jar tomcat@$TargetServersList:/usr/local/tomcat/webapps/
+echo "### Completed - Copying WAR file(s) to the server : $TargetServer ###" 
